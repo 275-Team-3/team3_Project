565c331312caa4213a711448f5538b1303290e41
patelsap <patelsap@udel.edu>
2023-04-15 13:10:55 -0400

    progress on typescript form

--- src/components/makeaccount/makeaccount.tsx
+++ src/components/makeaccount/makeaccount.tsx
@@ -3,43 +3,50 @@ import React from "react";
 function Makeaccount(): JSX.Element {
     return (
         <div className="App">
-
-
-
-
-            <form action="action_page.php" style="border:1px solid #ccc">
-                <div className="container">
-                    <h1>Sign Up</h1>
-                    <p>Please fill in this form to create an account.</p>
-                    <hr>
-
-                    <label htmlFor="email"><b>Email</b></label>
-                    <input type="text" placeholder="Enter Email" name="email" required>
-
-                    <label htmlFor="psw"><b>Password</b></label>
-                    <input type="password" placeholder="Enter Password" name="psw" required>
-
-                    <label for="psw-repeat"><b>Repeat Password</b></label>
-                    <input type="password" placeholder="Repeat Password" name="psw-repeat" required>
-
-                    <label>
-                    <input type="checkbox" checked="checked" name="remember" style="margin-bottom:15px"> Remember me
-                    </label>
-
-                    <p>By creating an account you agree to our <a href="#" style="color:dodgerblue">Terms & Privacy</a>.</p>
-
-                    <div class="clearfix">
-                    <button type="button" class="cancelbtn">Cancel</button>
-                    <button type="submit" class="signupbtn">Sign Up</button>
-                    </div>
-                </div>
-            </form>
-
-
-
-
+            <h2>landing page test</h2>
+            <p>hello there</p>
 
             
+            <button onClick="document.getElementById('id01').style.display='block'">Sign Up</button>
+
+             <div id="id01" className="modal">
+            <span onClick="document.getElementById('id01').style.display='none'" class="close" title="Close Modal">times;</span>
+            <form className="modal-content" action="/action_page.php">
+                <div className="container">
+                <h1>Sign Up</h1>
+                <p>Please fill in this form to create an account.</p>
+                <hr>
+                <label htmlFor="email"><b>Email</b></label>
+                <input type="text" placeholder="Enter Email" name="email" required></input>
+
+                <label htmlFor="psw"><b>Password</b></label>
+                <input type="password" placeholder="Enter Password" name="psw" required></input>
+
+                <label htmlFor="psw-repeat"><b>Repeat Password</b></label>
+                <input type="password" placeholder="Repeat Password" name="psw-repeat" required></input>
+
+                <label>
+                    <input type="checkbox" checked="checked" name="remember" style="margin-bottom:15px"> Remember me </input>
+                </label>
+
+                <p>By creating an account you agree to our <a href="#" style="color:dodgerblue">Terms and Privacy</a>.</p>
+
+                <div className="clearfix">
+                    <button type="button" onClick="document.getElementById('id01').style.display='none'" class="cancelbtn">Cancel</button>
+                    <button type="submit" className="signup">Sign Up</button>
+                </div>
+                </div>
+            </form>
+            </div>
+
+
+
+
+
+
+
+
+
         </div>
     );
 }
