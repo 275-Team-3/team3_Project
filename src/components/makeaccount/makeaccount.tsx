7e75479318e95a9f46abcdd6b2ab2894b3990436
patelsap <patelsap@udel.edu>
2023-04-19 10:20:21 -0400

    reset code for makeaccount, deleted styles page

--- src/components/makeaccount/makeaccount.tsx
+++ src/components/makeaccount/makeaccount.tsx
@@ -5,40 +5,6 @@ function Makeaccount(): JSX.Element {
         <div className="App">
             <h2>Account Creation </h2>
             <p>Please click the button below to create account.</p>
-
-            {/* 
-            <button onClick="document.getElementById('id01').style.display='block'">Sign Up</button>
-
-             <div id="id01" className="modal">
-            <span onClick="document.getElementById('id01').style.display='none'" class="close" title="Close Modal">times;</span>
-            <form className="modal-content" action="/action_page.php">
-                <div className="container">
-                <h1>Sign Up</h1>
-                <p>Please fill in this form to create an account.</p>
-                <hr>
-                <label htmlFor="email"><b>Email</b></label>
-                <input type="text" placeholder="Enter Email" name="email" required></input>
-
-                <label htmlFor="psw"><b>Password</b></label>
-                <input type="password" placeholder="Enter Password" name="psw" required></input>
-
-                <label htmlFor="psw-repeat"><b>Repeat Password</b></label>
-                <input type="password" placeholder="Repeat Password" name="psw-repeat" required></input>
-
-                <label>
-                    <input type="checkbox" checked="checked" name="remember" style="margin-bottom:15px"> Remember me </input>
-                </label>
-
-                <p>By creating an account you agree to our <a href="#" style="color:dodgerblue">Terms and Privacy</a>.</p>
-
-                <div className="clearfix">
-                    <button type="button" onClick="document.getElementById('id01').style.display='none'" class="cancelbtn">Cancel</button>
-                    <button type="submit" className="signup">Sign Up</button>
-                </div>
-                </div>
-            </form>
-            </div>
-             */}
         </div>
     );
 }
