aaa60640eac2747a383b428d9f195d376bba1191
UBON-235 <114717478+UBON-235@users.noreply.github.com>
2023-05-08 21:02:42 -0400

    Revert "Updating"

--- src/components/admininventory/ProductData.ts
+++
@@ -1,9 +0,0 @@
-export interface ProductData {
-    title: string;
-    description: string;
-    price: number;
-    stock: number;
-    brand: string;
-    category: string;
-    image: string;
-}
